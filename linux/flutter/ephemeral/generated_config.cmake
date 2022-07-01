a2f93f2407854d546c204acef4dfab3bd303ab3f
john <johnosezele@gmail.com>
2022-07-01 16:41:30 +0100

    app icon

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/john/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/john/food_delivery" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/john/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/john/food_delivery"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/john/food_delivery/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/john/food_delivery/lib/main.dart"
+)
