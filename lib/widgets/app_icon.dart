7c86af8e3149696954e986568f2c9d51ca1d0ee2
john <johnosezele@gmail.com>
2022-07-01 16:35:34 +0100

    appIcon done

--- lib/widgets/app_icon.dart
+++ lib/widgets/app_icon.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/cupertino.dart';
 
 class AppIcon extends StatelessWidget {
-  const AppIcon({Key? key}) : super(key: key);
+  final IconData icon;
+  final Color backgroundColor;
+  final Color iconColor;
+  final double size;
+  const AppIcon(
+      {Key? key,
+      required this.icon,
+      this.backgroundColor = const Color(0xFFfcf4e4),
+      this.iconColor = const Color(0xFF756d54),
+      this.size = 40})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
